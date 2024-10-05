2d4e298007acee006c2c51c21256994c64f69c9e
Dilhannadun <dilhannadun777@gmail.com>
2024-10-05 17:51:23 +0530

    added exo planets

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,13 +1,16 @@
-import { useRef, useLayoutEffect } from "react";
+import { useRef, useLayoutEffect, useMemo } from "react";
 import { useThree } from "@react-three/fiber";
 import { BlackHole } from "./BlackHole";
 import Earth from "./Earth";
-
+import ExoPlanet from "./ExoPlanet";
 import * as THREE from "three";
+import ExoPlanetType from "../types/ExoPlanetType";
 
-type Props = {};
+type Props = {
+  exoPlanets: ExoPlanetType[] | undefined;
+};
 
-function Scene({}: Props) {
+function Scene({ exoPlanets }: Props) {
   const gl = useThree((state) => state.gl);
   const groupRef = useRef<THREE.Group>(null);
   const blackHoleRef = useRef<THREE.Mesh>(null);
@@ -18,12 +21,25 @@ function Scene({}: Props) {
     gl.setPixelRatio(window.devicePixelRatio);
   }, [gl]);
 
-  // Use Web Workers to filter and process planets
+  // Memoized rendering of exoplanets based on calculated data
+  const allExoPlanets = useMemo(() => {
+    return exoPlanets?.map((planet, index) => {
+      return (
+        <ExoPlanet
+          key={index}
+          planet={planet}
+          color={"darkred"}
+          snrValue={planet.snr}
+        />
+      );
+    });
+  }, [exoPlanets]);
 
   return (
     <group ref={groupRef}>
       <BlackHole ref={blackHoleRef} />
       <Earth ref={earthRef} color={"blue"} />
+      {allExoPlanets}
     </group>
   );
 }
