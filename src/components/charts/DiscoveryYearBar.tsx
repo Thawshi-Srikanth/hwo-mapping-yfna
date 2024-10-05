b1353328f0a10748af297f92ba30b7ce4a1e5231
Thawshi Srikanth <thawshi.dev@protonmail.com>
2024-10-05 18:16:08 +0530

    added var filters

--- src/components/charts/DiscoveryYearBar.tsx
+++ src/components/charts/DiscoveryYearBar.tsx
@@ -20,29 +20,42 @@ ChartJS.register(
   Title
 );
 
+const discoveryMethods = [
+  { method: "Transit", color: "#8884d8b3", shape: "circle" },
+  { method: "Radial Velocity", color: "#ff6384b3", shape: "triangle" },
+  { method: "Transit Timing Variations", color: "#36a2ebb3", shape: "rect" },
+  { method: "Imaging", color: "#ffce56b3", shape: "star" },
+  {
+    method: "Orbital Brightness Modulation",
+    color: "#4bc0c0b3",
+    shape: "cross",
+  },
+  { method: "Solar System", color: "#9966ffb3", shape: "rectRot" },
+];
+
 const DiscoveryYearBar = ({ data }: { data: ExoPlanetType[] }) => {
   const currentYear = new Date().getFullYear();
 
-  // Calculate counts based on discovery years
   const yearCount = data.reduce((acc, planet) => {
     if (planet.disc_year <= currentYear) {
-      acc[planet.disc_year] = (acc[planet.disc_year] || 0) + 1;
+      acc[planet.disc_year] = acc[planet.disc_year] || {};
+      acc[planet.disc_year][planet.discoverymethod] =
+        (acc[planet.disc_year][planet.discoverymethod] || 0) + 1;
     }
     return acc;
-  }, {} as Record<number, number>);
+  }, {} as Record<number, Record<string, number>>);
 
   const chartData = {
-    labels: Object.keys(yearCount).map((year) => year),
-    datasets: [
-      {
-        label: "Number of Discoveries",
-        data: Object.keys(yearCount).map((year) => yearCount[parseInt(year)]),
-        backgroundColor: "#36a2eb",
-        stack: "stacked",
-      },
-    ],
+    labels: Object.keys(yearCount),
+    datasets: discoveryMethods.map(({ method, color }) => ({
+      label: method,
+      data: Object.keys(yearCount).map(
+        (year) => yearCount[parseInt(year)][method] || 0
+      ),
+      backgroundColor: color,
+      stack: "stacked",
+    })),
   };
-
   const options = {
     responsive: true,
     maintainAspectRatio: false,
