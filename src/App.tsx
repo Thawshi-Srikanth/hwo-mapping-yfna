5c14467752eb16d1b0acc01a4bb577d9ebac5097
Thawshi Srikanth <thawshi.dev@protonmail.com>
2024-10-05 12:22:16 +0530

    added preloader

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import PreLoader from "../src/ui/preLoader";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <PreLoader />
+    </>
+  );
 }
 
 export default App;
