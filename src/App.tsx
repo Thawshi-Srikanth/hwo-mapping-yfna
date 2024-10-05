9d24c4371bf9ec89f66c7aece61fb0560cd43587
Dilhannadun <dilhannadun777@gmail.com>
2024-10-05 14:12:17 +0530

    started 3D dev

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import Header from "./components/ui/Header";
 import ChartSideNav from "./components/ui/ChartsSideBar";
 import InfoSideNav from "./components/ui/InfoSideBar";
 import ToolsSideNav from "./components/ui/ToolsSideBar";
-
+import { Canvas } from "@react-three/fiber";
+import { Perf } from "r3f-perf";
+import SceneBackground from "./components/SceneBackground";
 function App() {
   const [isToolSideBarOpen, setIsToolSideBarOpen] = useState(false);
   const [isInfoSideBarOpen, setIsInfoSideBarOpen] = useState(false);
@@ -37,6 +39,11 @@ function App() {
         toggleSideNav={toggleInfoBarSideBar}
       />
       <ChartSideNav isOpen={isChartNavOpen} toggleSideNav={toggleChartNav} />
+
+      <Canvas gl={{ antialias: false }}>
+        <Perf position="bottom-right" />
+        <SceneBackground texturePath="/images/background/stars_8k.webp" />
+      </Canvas>
     </div>
   );
 }
