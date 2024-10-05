819d51335d74c0606750b1ba51dd8236cc54c8f9
Dilhannadun <dilhannadun777@gmail.com>
2024-10-05 22:38:30 +0530

    vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"), // Alias setup
+    },
+  },
+  worker: {
+    format: "es", // Use ES modules for workers
+  },
+});
